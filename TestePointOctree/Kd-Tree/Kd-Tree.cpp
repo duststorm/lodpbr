ac8f6cbf6711b32558e08b600f461e6b8c27dc86
felipe.celer <felipe.celer@ad75c2bb-4144-0410-81b1-f1d0baf8b1ac>
2008-08-28 16:25:41 +0000

    Adicionado Kd-Tree

---
+++ TestePointOctree/Kd-Tree/Kd-Tree.cpp
@@ -0,0 +1,12 @@
+/**
+ * Implementation of a 2d-Tree
+ * Nearest k neighbors search algorithm.
+ *
+ * Author : Ricardo Marroquim
+ * Date created : 08-01-2007
+ *
+ **/
+
+#include "kd-tree.h"
+
+using namespace std;
