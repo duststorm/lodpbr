e5bb97cb6c3a848d38f2cd37552a8d7eddfcaf96
felipe.celer <felipe.celer@ad75c2bb-4144-0410-81b1-f1d0baf8b1ac>
2009-11-05 21:41:29 +0000

    log onde fica ?

--- trunk/lodpbr/Qt/myMainWindow.cpp
+++ trunk/lodpbr/Qt/myMainWindow.cpp
@@ -10,29 +10,7 @@
 
 using namespace std;
 
-MyMainWindow::MyMainWindow (QMainWindow *parent): QMainWindow(parent)
-{
-    setupUi (this);
 
-
-    glWidget = new GLWidget(this);
-    //       glWidget->show();
-    //
-    //		 WidgetProxy *proxy = new WidgetProxy(glWidget,this);
-
-    mdiArea->addSubWindow(glWidget);
-
-    //     QWidget* w = proxy->createWindow("Embedded window");
-    //     w->move(20, 270);
-    //     w->layout()->addWidget(new QLineEdit(this));
-    //     QPushButton* pb = new QPushButton("Create new toplevel GL widget", this);
-    //     w->layout()->addWidget(pb);
-    //     proxy->addWidget(w);
-
-
-
-
-}
 
 //void MyMainWindow::on_doubleSpinBoxThreshold_valueChanged(double d)
 //{
--- trunk/lodpbr/Qt/myMainWindowInit.cpp
+++ trunk/lodpbr/Qt/myMainWindowInit.cpp
@@ -1,5 +1,28 @@
 #include "myMainWindow.hpp"
 
+
+MyMainWindow::MyMainWindow (QMainWindow *parent): QMainWindow(parent)
+{
+    setupUi (this);
+
+    glWidget = new GLWidget(this);
+
+    //       glWidget->show();
+    //
+    //		 WidgetProxy *proxy = new WidgetProxy(glWidget,this);
+
+    mdiArea->addSubWindow(glWidget);
+
+    //     QWidget* w = proxy->createWindow("Embedded window");
+    //     w->move(20, 270);
+    //     w->layout()->addWidget(new QLineEdit(this));
+    //     QPushButton* pb = new QPushButton("Create new toplevel GL widget", this);
+    //     w->layout()->addWidget(pb);
+    //     proxy->addWidget(w);
+
+}
+
+
 void MyMainWindow::on_action_Cluster_Debug_triggered()
 {
 	if (action_Cluster_Debug->isChecked())
