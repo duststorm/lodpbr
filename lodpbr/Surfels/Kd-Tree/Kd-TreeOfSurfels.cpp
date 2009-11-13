5126f8b515f0a0c1090ba972bbba0d1c9e61e9bb
felipe.celer <felipe.celer@ad75c2bb-4144-0410-81b1-f1d0baf8b1ac>
2009-11-13 16:56:52 +0000

    ....

---
+++ lodpbr/Surfels/Kd-Tree/Kd-TreeOfSurfels.cpp
@@ -0,0 +1,12 @@
+/**
+ * Implementation of a 2d-Tree
+ * Nearest k neighbors search algorithm.
+ *
+ * Author : Ricardo Marroquim
+ * Date created : 08-01-2007
+ *
+ **/
+
+#include "Kd-Tree.hpp"
+
+using namespace std;
